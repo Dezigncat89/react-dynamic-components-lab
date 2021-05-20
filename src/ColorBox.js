5ab3295878b1a093442995866e23433ec8ff5e8c
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-20 09:07:05 -0700

    Update ColorBox.js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,49 @@
-import React, { Component } from 'react';
 
-export default class ColorBox extends Component {
+import React, {Component} from 'react';
 
-  state = {
-    todos: [
-      
-    ]
+
+class ColorBox extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      anyBoxes: []
+    }
+    this.make = this.make.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+
+  delete(id) {
+    this.setState({
+      anyBoxes: this.state.anyBoxes.filter(aBox => aBox.id !== id)
+    });
+  }
+
+  make(secondBox) {
+    this.setState({ anyBoxes: [...this.state.anyBoxes, secondBox] })
   }
 
   render() {
+    const anyBoxes = this.state.anyBoxes.map(aBox => 
+      <aBox 
+        key={aBox.id}
+        id={aBox.id}
+        width={aBox.width} 
+        height={aBox.height} 
+        color={aBox.color} 
+        deleteBox={this.delete}
+      />
+    )
+
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div>
+        <h1>Create a Color Box</h1>
+        <NewBoxesForm makeBox={this.make} />
+        {anyBoxes}
       </div>
     )
   }
-
 }
+
+export default ColorBox;
 
