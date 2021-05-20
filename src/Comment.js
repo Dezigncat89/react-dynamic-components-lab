d92f5e5d75f0121ee5c4f1167201cd99f3069b6a
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-20 08:44:09 -0700

    Update Comment.js

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,17 @@
-import React from "react"
+import React, { Component } from 'react'
+import BlogPost from "./src/BlogPost"
+import App from "./src/App
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className ="comment">
+        {this.props.commentText}
+      </div>
+    )
+  }
+}
+
+export default Comment
+
+
